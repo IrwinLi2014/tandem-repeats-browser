7e02b2b11af5c7f2a8d740e357825a24e9950bc4
Ubuntu <ubuntu@ip-172-31-13-46.ec2.internal>
2017-04-30 23:31:38 +0000

    upload

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import web
 import shutil
+import subprocess
 render = web.template.render('webpage/')
 
 urls = (
@@ -11,28 +12,29 @@ class index:
     def GET(self):
         return render.index()
 
+
+#class Upload:
+#    def GET(self):
+#        return """<html><head></head><body>
+#<form method="POST" enctype="multipart/form-data" action="">
+#<input type="file" id="myfile "name="myfile" />
+#<br/>
+#<input type="submit" />
+#</form>
+#</body></html>
+
     def POST(self):
-        x = web.input(input_file={})
-	if x.input_file.file:
-            destFile = open('./input', 'wb')
-            destFile.write(x.input_file.file.read())
+        x = web.input(myfile={})
+	if x.myfile.file:
+            destFile = open('./input.fna', 'wb')
+            destFile.write(x.myfile.file.read())
             destFile.close()
-        web.debug(x['input_file'].filename) # This is the filename
-        web.debug(x['input_file'].value) # This is the file contents
-        web.debug(x['input_file'].file.read()) # Or use a file(-like) object
-        raise web.seeother('/upload')
-
-
-class Upload:
-    def GET(self):
-        return """<html><head></head><body>
-<form method="POST" enctype="multipart/form-data" action="">
-<input type="file" id="myfile "name="myfile" />
-<br/>
-<input type="submit" />
-</form>
-</body></html>"""
-
+            command = "python3 ./tandem.py -m 0 -w 1000 -i input.fna"
+            subprocess.call(command, shell=True)
+        #web.debug(x['myfile'].filename) # This is the filename
+        #web.debug(x['myfile'].value) # This is the file contents
+        #web.debug(x['myfile'].file.read()) # Or use a file(-like) object
+	raise web.seeother('/')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
