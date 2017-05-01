7ee6a9a0ecef71891a6b235205da05564d844593
Chuchu Ding <c.ding@columbia.edu>
2017-04-30 20:11:17 -0400

    index.html areas added

--- code.py
+++ code.py
@@ -24,16 +24,19 @@ class index:
 #</body></html>
 
     def POST(self):
-        x = web.input(myfile={})
-	if x.myfile.file:
+        x = web.input(myfile={}, alpha='ATCG')
+        command = "python3 ./tandem.py -m %s -w %s -a %s -i input.fna", (x.maxtolerance, x.windowsize, x.alpha)
+        if x.fileyes and x.myfile.file:
             destFile = open('./input.fna', 'wb')
             destFile.write(x.myfile.file.read())
             destFile.close()
-            command = "python3 ./tandem.py -m 0 -w 1000 -i input.fna"
-            subprocess.call(command, shell=True)
+        if x.cutpasteyes:
+            command = "python3 ./tandem.py -m %s -w %s -a %s -s %s", (x.maxtolerance, x.windowsize, x.alpha, x.seq)
+        subprocess.call(command, shell=True)
         #web.debug(x['myfile'].filename) # This is the filename
         #web.debug(x['myfile'].value) # This is the file contents
         #web.debug(x['myfile'].file.read()) # Or use a file(-like) object
+
 	raise web.seeother('/')
 
 if __name__ == "__main__":
