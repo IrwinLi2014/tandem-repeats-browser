5b73b8fde8c85ca5b822b877da57d8335d12acf7
Ubuntu <ubuntu@ip-172-31-13-46.ec2.internal>
2017-05-01 06:09:40 +0000

    ,,

--- code1.py
+++ code1.py
@@ -80,20 +80,19 @@ class repeat:
             </head>
             <body>
             <pre><b>search repeat (using index):</b></pre>
-            <form method="POST" enctype="multipart/form-data" action=""><textarea name="rn" rows=1 cols=10 id="rn"></textarea><br><input type="submit" /></form>
-            <pre><b>search resupt (using index):</b></pre>
+            <form method="POST" enctype="multipart/form-data" action=""><textarea name="rn" rows=1 cols=10 id="rn"></textarea><input type="submit" /></form>
+            <pre><b>search result:</b></pre>
             <ul><li><a href='"+str(n)+"'>[ index , start point of repeat, end point of the 1st pattern , end point of repeat " ] </a></li></ul>
             """
     page_body = []
     page_tail = "</body></html>"
     def GET(self):
         self.page_body = []
-        self.page_body.append("""<ul><li><a href='"+str(n)+"'>[ index , start point of repeat, end point of the 1st pattern , end point of repeat " ] </a></li></ul>""")
         with open('out.csv', 'rb') as csvfile:
             repeats = csv.reader(csvfile, delimiter=',')
             n = 0
             for row in repeats:
-                r = "<ul><li><a href='"+str(n)+"'>[  #" + n + " , " + row[0] + " , " +row[1] + " , " + row[2] + " ] </a></li></ul>"
+                r = "<ul><li><a href='"+str(n)+"'>[  #" + str(n) + " , " + row[0] + " , " +row[1] + " , " + row[2] + " ] </a></li></ul>"
                 self.page_body.append(r)
                 n+=1
         return (self.page_head + " ".join(self.page_body) + self.page_tail)
