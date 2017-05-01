c158e8405f28bcf10588400a69ba4144e0cc205f
Ubuntu <ubuntu@ip-172-31-13-46.ec2.internal>
2017-05-01 17:42:06 +0000

    minor changes

--- server1.py
+++ server1.py
@@ -45,7 +45,7 @@ class index:
         if x['fileselect']=="Cut and paste sequence":
             gfastayes = False
             command = "python3 ./tandem.py -m " + x.maxtolerance.encode('ascii','ignore') + " -w " + x.windowsize.encode('ascii','ignore') + " -a " + alph + " -s " + x.seq.encode('ascii','ignore') + " -b " + bond
-        print('command', command)
+        # print('command', command)
         subprocess.call(command, shell=True)
         raise web.seeother('/repeat')
 
@@ -60,8 +60,6 @@ class repeat:
                 overflow: hidden;
                 background-color: #E0E0E0;
             }
-            h1   {background-color:Grey; text-align:center; font-size:300%; color:white; font-family:verdana;}
-            h2   {background-color:Grey; text-align:center; font-size:200%; color:white; font-family:verdana;}
 
             li a {
                 display: block;
@@ -69,15 +67,12 @@ class repeat:
                 text-align: center;
                 padding: 16px;
                 text-decoration: none;
-                font-family:verdana;
             }
 
             li a:hover {
                 background-color: #FFFFCC;
             }
             pre {
-                word-wrap: break-word;
-                width: 100px;
                 color:DarkBlue;
                 font-size:120%;
                 font-family:courier;
@@ -85,8 +80,6 @@ class repeat:
             </style>
             </head>
             <body>
-            <h1>Tandem Repeats Found</h1>
-            <h2><a href='/'>Back</a></h2>
             <pre><b>search repeat (using index):</b></pre>
             <form method="POST" enctype="multipart/form-data" action=""><textarea name="rn" rows=1 cols=10 id="rn"></textarea><input type="submit" /></form>
             <pre><b>search result:</b></pre>
@@ -97,21 +90,15 @@ class repeat:
     def GET(self):
         self.page_body = []
         global repeatfound
-        print("repeat: ", repeatfound)
-        data = web.input(id=-1)
-        if (data.id!=-1):
-            command = "python3 ./showrepeats.py -rn " + data.id + " -m " + str(gm) + " -w " + str(gw) + " -a " + str(ga) + " -b " + str(gb) + " -i input.fna > repeat_found"
-            subprocess.call(command, shell=True)
-            with open("repeat_found", 'rb') as f:
-                repeatfound = f.read()
+        # print("repeat: ", repeatfound)
         if (repeatfound):
             self.page_body.append("""<pre><b>""" + repeatfound +  """</b></pre>""")
-        self.page_body.append("""<ul><li><a href='/repeat'><b>[ index , start point of repeat, end point of the 1st pattern , end point of repeat " ]</b> </li></ul>""")
+        self.page_body.append("""<ul><li><a href='"+str(n)+"'>[ index , start point of repeat, end point of the 1st pattern , end point of repeat " ] </a></li></ul>""")
         with open('out.csv', 'rb') as csvfile:
             repeats = csv.reader(csvfile, delimiter=',')
             n = 0
             for row in repeats:
-                r = "<ul><li><a href='/repeat?id=" + str(n)+ "''>[ " + str(n) + " , " + row[0] + " , " +row[1] + " , " + row[2] + " ] </a></li></ul>"
+                r = "<ul><li><a href=n>[  #" + str(n) + " , " + row[0] + " , " +row[1] + " , " + row[2] + " ] </a></li></ul>"
                 self.page_body.append(r)
                 n+=1
         return (self.page_head + " ".join(self.page_body) + self.page_tail)
@@ -127,7 +114,7 @@ class repeat:
             subprocess.call(command, shell=True)
             with open("repeat_found", 'rb') as f:
                 repeatfound = f.read()
-            print(repeatfound)
+            # print(repeatfound)
         
         raise web.seeother('/repeat')
 
